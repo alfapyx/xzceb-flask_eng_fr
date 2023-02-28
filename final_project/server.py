3280c34f97597f0f3a7e26af2adcac00ecaa5703
alfapyx <alfapyx@gmail.com>
2023-02-28 10:05:51 -0500

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -7,18 +7,18 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    french_tr = translator.english_to_french(textToTranslate)
+    return french_tr
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    engl_tr = translator.french_to_english(textToTranslate)
+    return engl_tr
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
